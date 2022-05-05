0851e6cb588b72686d8f48e3bba78bcf80c93d04
Lauren McCall <lmmccolloch@gmail.com>
2022-05-05 18:08:07 -0400

    Landing page routed/ css added/ pet data fetched from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import { Landing } from './pages/Landing'
+
+export type PetType = {
+  id: number
+  name: string
+  birthday: string
+  hungerLevel: number
+  happinessLevel: number
+  lastInteractedWithDate: string
+  isDead: boolean
+}
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <header>Welcome!</header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+        </Routes>
+      </main>
+    </div>
+  )
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { PetType } from '../App'
+
+// The home page should show a list of all the
+// pets in your API. The listing should include their name,
+// birthday, hunger level, and happiness level.
+export function Landing() {
+  const [pets, setPets] = useState<PetType[]>([])
+
+  function getPetList() {
+    async function fetchPetList() {
+      const response = await axios.get(
+        'https://tamagotchilmccall.herokuapp.com/api/pets'
+      )
+      if (response.status === 200) {
+        setPets(response.data)
+        console.log(response.data)
+      }
+    }
+    fetchPetList()
+  }
+  useEffect(getPetList, [])
+  // console.log(getPetList)
+
+  return (
+    <div>
+      <article>
+        {pets.map((pets) => {
+          return (
+            <ul key={pets.id}>
+              <li>Name: {pets.name}</li>
+              <li>Birthday: {new Date(pets.birthday).toLocaleDateString()}</li>
+              <li>Hunger Level: {pets.hungerLevel}</li>
+              <li>Happiness Level: {pets.happinessLevel}</li>
+            </ul>
+          )
+        })}
+      </article>
+    </div>
+  )
+}
